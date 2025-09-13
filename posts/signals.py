c4bd5540dbb97f0309ed3862c714d7082046d709
San Rain <mgsanjuan@student.hau.edu.ph>
2025-09-13 10:24:42 +0800

    wip

--- posts/signals.py
+++ posts/signals.py
@@ -1,6 +1,10 @@
 import random
 import string
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils.text import slugify
+from posts.models import Post
+
 
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -17,3 +21,9 @@ def unique_slug_generator(instance, new_slug=None):
         new_slug = f"{slug}-{random_string_generator(size=4)}"
         return unique_slug_generator(instance, new_slug=new_slug)
     return slug
+
+@receiver(pre_save, sender=Post)
+def add_slug_to_post(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        print(f"New slug: {instance.content[:30]}")
+        instance.slug = unique_slug_generator(instance)
